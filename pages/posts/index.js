bc6ac3c47bdcf483d7d2e91381cde96b1930d454
KhoaPC <luongvankhoapc@gmail.com>
2022-12-30 05:44:25 +0700

    22Dec30 MISC

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,27 +1,30 @@
 import Link from "next/link";
 
 export const getStaticProps = async () => {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-    const data = await res.json();
-  
-    return { props: { posts: data} };
-  };
-  
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const data = await res.json();
 
-function PostList({posts}) {
-    return ( <>
-    <h1>List of posts</h1>
-    {posts.map(post => {
+  return { props: { posts: data } };
+};
+
+function PostList({ posts }) {
+  return (
+    <>
+      <h1>List of posts</h1>
+      {posts.map((post) => {
         return (
-            <div>
-                <Link href={`posts/${post.id}`}>
-                <h4>{post.id} : {post.title}</h4>
-                <hr/>
-                </Link>
-            </div>
-        )
-    })}
-    </> );
+          <div>
+            <Link href={`posts/${post.id}`}>
+              <h4>
+                {post.id} : {post.title}
+              </h4>
+              <hr />
+            </Link>
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default PostList;
