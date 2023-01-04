875136a5b6428c4ca012f64c649189e1ce4c1591
KhoaPC <luongvankhoapc@gmail.com>
2023-01-04 07:35:16 +0700

    23Jan04 KB method `GET` & `POST` data to file directory

--- pages/api/comments/index.js
+++ pages/api/comments/index.js
@@ -1,16 +1,48 @@
-import { comments } from '../../../data/comments'
+import crypto from "crypto";
+import { getCommentsFromJSON } from "../../../lib/comments-helpers";
+import path from "path";
+import { promises as fs } from "fs";
 
-export default function handler(req, res) {
-  if (req.method === 'GET') {
-    res.status(200).json(comments)
-  } else if (req.method === 'POST') {
-    const comment = req.body.comment
-    const newComment = {
-      id: Date.now(),
-      text: comment
-    }
-    comments.push(newComment)
-    res.status(201).json(newComment)
-  }
-}
+export default async function handler(req, res) {
+  const { url, method, query, body, headers } = req;
 
+  const jsonDirectory = path.join(process.cwd(), "json");
+  const jsonFile = jsonDirectory + "/comments.json";
+
+  switch (method) {
+    case "GET":
+      const comments = await getCommentsFromJSON();
+
+      res.status(200).json(comments);
+      break;
+
+    case "POST":
+      let commentsCurrent = await getCommentsFromJSON();
+
+      const { comment } = body;
+
+      // Check if valid comment
+      if (!comment || comment.trim().length < 1) return res.status(400).send('hello');
+      if (commentsCurrent.length >= 50) return res.status(400).send('hello');
+
+      // ok
+      const newComment = {
+        id: crypto.randomUUID(),
+        text: comment,
+      };
+
+      // Push to comments
+      commentsCurrent.push(newComment);
+
+      const commentsStringified = JSON.stringify(commentsCurrent);
+      // Write to file
+      await fs.writeFile(jsonFile, commentsStringified, "utf-8");
+
+      res.status(201).json(newComment); // ok
+
+      break;
+
+    default:
+      break;
+  } // switch
+} // handler
