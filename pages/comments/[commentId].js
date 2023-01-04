a03b4502981524b6c77a9ca5703cf860414aeb66
KhoaPC <luongvankhoapc@gmail.com>
2023-01-04 07:32:03 +0700

    23Jan04 MISC

--- pages/comments/[commentId].js
+++ pages/comments/[commentId].js
@@ -1,42 +1,37 @@
-import { comments } from '../../data/comments'
-
+import { comments } from "../../data/comments";
 
 export async function getStaticProps(context) {
-  const { params } = context
-  const { commentId } = params
+  const { params } = context;
+  const { commentId } = params;
 
-  const comment = comments.find(comment => comment.id === parseInt(commentId))
-  console.log(comment)
-
-  /** Don't do this 
-  const response = await fetch(`http:localhost:3000/api/comments/${commentId}`)
-  const data = await response.json()
-  */
+  const comment = comments.find(
+    (comment) => comment.id === parseInt(commentId)
+  );
 
   return {
     props: {
-      comment
-    }
-  }
+      comment,
+    },
+  };
 }
 
 export async function getStaticPaths() {
   return {
     paths: [
-      { params: { commentId: '1' } },
-      { params: { commentId: '2' } },
-      { params: { commentId: '3' } }
+      { params: { commentId: "1" } },
+      { params: { commentId: "2" } },
+      { params: { commentId: "3" } },
     ],
-    fallback: false
-  }
+    fallback: false,
+  };
 }
 
 function Comment({ comment }) {
   return (
     <div>
-      {comment.id}. {comment.text}
+      {comment.id}: {comment.text}
     </div>
-  )
+  );
 }
 
-export default Comment
+export default Comment;
