2f98e74490d771f4c3e7fd1f052079ad6fe91a57
KhoaPC <luongvankhoapc@gmail.com>
2023-01-04 07:34:36 +0700

    23Jan04 KB method`DELETE` content file directory

--- pages/api/comments/[commentId].js
+++ pages/api/comments/[commentId].js
@@ -1,20 +1,31 @@
-import { comments } from "../../../data/comments";
+import path from "path";
+import { promises as fs } from "fs";
+import { getCommentsFromJSON } from "../../../lib/comments-helpers";
 
-export default function handler(req, res) {
-  const { commentId } = req.query;
-  if (req.method === "GET") {
-    const comment = comments.find(
-      (comment) => comment.id === parseInt(commentId)
-    );
-    res.status(200).json(comment);
-  } else if (req.method === "DELETE") {
-    const deletedComment = comments.find(
-      (comment) => comment.id === parseInt(commentId)
-    );
-    const index = comments.findIndex(
-      (comment) => comment.id === parseInt(commentId)
-    );
-    comments.splice(index, 1);
-    res.status(200).json(deletedComment);
-  }
-}
+export default async function handler(req, res) {
+  const { url, method, query, body, headers } = req;
+
+  const jsonDirectory = path.join(process.cwd(), "json");
+  const jsonFile = jsonDirectory + "/comments.json";
+
+  switch (method) {
+    case "DELETE":
+      let commentsCurrent = await getCommentsFromJSON();
+      const { commentId } = query;
+
+      // Remove from comments
+      const commentsNew = commentsCurrent.filter((item) => {
+        return item.id != commentId;
+      });
+
+      const commentsStringified = JSON.stringify(commentsNew);
+      // Write to file
+      await fs.writeFile(jsonFile, commentsStringified, "utf-8");
+
+      res.status(201).send(commentId);
+      break;
+
+    default:
+      break;
+  } // switch
+} // handler
